20e3d705bd6d6b0e88aa3916a54c65310199dfbd
krislenekylieBN <krislenekyliebn@gmail.com>
2024-02-13 15:39:24 +0800

    kylie so pro

--- src/Cancel/Cancel.jsx
+++ src/Cancel/Cancel.jsx
@@ -1,6 +1,26 @@
 import React from 'react'
+import axios from 'axios';
+import { useState } from 'react'
+import { useEffect } from 'react';
+import { FaEdit } from 'react-icons/fa';
 
 function Cancel() {
+  const [reservations, setReservations] = useState([]);
+
+  useEffect(() => {
+    const getReservations = async () => {
+      try {
+        const response = await axios.get("http://127.0.0.1:1337/api/get-all-reservations");
+        setReservations(response.data.data); // Access data property
+      } catch (error) {
+        console.error("Error fetching reservations:", error);
+      }
+    };
+
+    getReservations();
+  }, []);
+
+
   return (
     <div className='container'>
         <div className='back-container'>
@@ -10,9 +30,33 @@ function Cancel() {
             <div className='reserve-container'>
                 Reserved Tickets
             </div>
-            <div className='ticket-container'>
-
-            </div>
+            <br></br>
+            <table className='ticket-container'>
+              <thead>
+                <tr>
+                  <th>Title</th>
+                  <th>Airing Time</th>
+                  <th>Seat</th>
+                  <th>Total</th>
+                  <th>Senior Citizen</th>
+                  <th>PWD</th>
+                  <th>Edit</th>
+                </tr>
+              </thead>
+              <tbody>
+                {reservations.map(reservation => (
+                  <tr key={reservation._id}>
+                    <td>{reservation.mov_ID.title}</td>
+                    <td>{reservation.airing_time}</td>
+                    <td>{reservation.seat.map(seat => seat.seatNumber).join(', ')}</td>
+                    <td>{reservation.total_price}</td>
+                    <td>{reservation.discount.senior_citizen ? 'Yes' : 'No'}</td>
+                    <td>{reservation.discount.pwd ? 'Yes' : 'No'}</td>
+                    <td><button>Edit</button></td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
         </div>
     </div>
   )
--- src/Seat/Seat.jsx
+++ src/Seat/Seat.jsx
@@ -1,8 +1,38 @@
-import React from 'react';
 import SeatNumber from './SeatNumber';
 import { Button, Card } from 'antd';
+//kylie
+import { useState } from 'react';
+import axios, { Axios } from 'axios';
 
 function Seat() {
+  const [inputReserve, setInputReserve] = useState({
+    mov_ID: "",
+    start_time: "",
+    tickets: "",
+    seats: "",
+    subtotal: "",
+    discount: "",
+    total: "",
+  });
+
+  // const handleInputChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setInputReserve((prevData) => ({
+  //     ...prevData,
+  //     [name]: value,
+  //   }))
+  // }
+
+  const postReserveClick=async() => {
+    try {
+      const response = await Axios.post('http://127.0.0.1:1337/api/addReservation', inputReserve);
+      console.log(response.data)
+    } catch (error) {
+      console.error('Error', error);
+    }
+  };
+
+
   return (
     <div className='seat-container' style={{ width: '80%', margin: 'auto' }}>
       <div className='title-container' style={{ textAlign: 'center' }}>
@@ -22,7 +52,7 @@ function Seat() {
         <h3 style={{marginBottom: '1em'}}>Subtotal:</h3>
         <h3 style={{marginBottom: '1em'}}> Discount:</h3>
         <h3 style={{marginBottom: '1em'}}>Total:</h3>
-        <Button type="primary" style={{ background: 'black', marginBottom: '1em', fontSize: '16px', width: '8em', height: '50px' }}>BUY NOW</Button>
+        <Button type="primary" onClick={postReserveClick} style={{ background: 'black', marginBottom: '1em', fontSize: '16px', width: '8em', height: '50px' }}>BUY NOW</Button>
         <div className='card-box' style={{ display: 'flex', alignItems: 'center' }}>
         <Card style={{ backgroundColor: '#f0f2f5', width: '3em', height: '3em', marginRight: '1em' }} />
         <h3>AVAILABLE</h3>
